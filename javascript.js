72a512119de68aac8095e3f176076029996c3cdf
leopoglia <92129999+leopoglia@users.noreply.github.com>
2021-11-18 21:51:47 -0300

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,27 +1,72 @@
 let body = document.querySelector('body');
 let modal = document.createElement('div');
 let modaldentro = document.createElement('div');
-let botao = document.createElement('div');
+let botao = document.createElement('button');
+botao.id = 'botaoAbrir';
 let botaoDENTRO = document.createElement('div');
-let titulomodal = document.createElement('div');
+let estilomodal = document.createElement('div');
+let erro = document.createElement('div');
+let enviado = document.createElement('div');
 
+
+let info = [];
 
 modal.id = 'modal';
+erro.id = 'erro'
+enviado.id = 'enviado'
+erro.innerHTML = "ESTA FALTANDO ALGO!"
+enviado.innerHTML = "CADASTRADO COM SUCESSO"
 modaldentro.id = 'modal_dentro';
 botaoDENTRO.id = 'divBOTAOFECHAR';
-
-modaldentro.appendChild(titulomodal);
-modal.appendChild(modaldentro)
 body.appendChild(botao);
+botao.onclick = abrir
+botao.innerHTML = "Abrir"
+
+modaldentro.appendChild(estilomodal);
+modal.appendChild(modaldentro)
 modaldentro.appendChild(botaoDENTRO);
-
-titulomodal.innerHTML = titulomodal.innerHTML + "<h1 id='titulomodal'> CADASTRE-SE </h1><p> <h3 id='texto'>Se cadastre no site agora, Gerencie suas informacoes, privacidade e seguranca para que a Cepexis Games atenda suas necessidades. Caso voce nao deseja se cadastrar melhor ja apertar no X ali em cima e sair da pagina, muito obrigado pela visita =)</h3><p> <input id='nomeModal' type='text' placeholder='Digite seu login...'></input>  <input id='senhaModal' type='password' placeholder='Digite sua senha...'></input><input id='dataNascimento' type='date' placeholder='Digite sua data De nascimento'> <button id='cadastro' onclick=cadastro() >Cadastrar</button>";
-botaoDENTRO.innerHTML = botaoDENTRO.innerHTML + "<button id='botaoFechar' onclick=fechar() >X</button>";
-botao.innerHTML = botao.innerHTML + "<button id='botaoAbrir' onclick=abrir() >Abrir</button>";
-
+estilomodal.innerHTML = estilomodal.innerHTML + "<h1 id='titulomodal'> CADASTRE-SE </h1><p><div class='estiloModal'><input id='nome' type='text' placeholder='Digite seu nome'></input>  <input id='sobrenome' type='text' placeholder='Digite seu sobrenome'></input><input id='dataNascimento' type='date' placeholder='Digite sua data De nascimento'> <button id='cadastro' onclick=cadastro() >Cadastrar</button><button id='botaoFechar' onclick=fechar() >Fechar</button></div>";
 
 function abrir(){
 body.appendChild(modal);
+}
+
+function cadastro(){
+    var nome1 = document.getElementById('nome').value;
+    var sobrenome1 = document.getElementById('sobrenome').value;
+    var data = document.getElementById('dataNascimento').value;
+
+
+    if ((!sobrenome1 || sobrenome1 == '') || (!nome1 || nome1 == '') || (!data || data == '')) {
+        estilomodal.appendChild(erro);
+        erro.style.cssText = 'background-color: red;' + 'color: white;';
+        enviado.style.cssText = 'box-shadow: none;' + 'user-select: none;' + 'width: 0;' + 'color: transparent;' + 'padding-left: 0px;' ;
+    }else{
+        erro.style.cssText = 'background-color: transparent;' + 'color: transparent;' + 'user-select: none;' + 'box-shadow: none;';
+        enviado.style.cssText = 'box-shadow: 0px 0px 60px rgb(49, 134, 0);' + 'width: 250;' + 'color: white' ;
+
+        estilomodal.appendChild(enviado);
+
+        let retornar = {
+            nome : nome,
+            sobrenome : sobrenome, 
+            data : data
+        }
+        
+
+        setTimeout(function(){
+            onclick=fechar();
+            },1000);
+
+        setTimeout(function(){ 
+            nome.value = ' ';
+            sobrenome.value = ' ';
+            dataNascimento.value = ' ';
+            }, 950);
+
+        info.push(retornar);    
+    }
+    
 }
 
 function fechar(){
